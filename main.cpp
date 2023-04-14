bfe8389319229fe6018216e40f11cf6018accdb6
祐介 戸田 <k022g0025@m.neec.ac.jp>
2023-04-14 15:40:51 +0900

    00_03まで実装

--- main.cpp
+++ main.cpp
@@ -1,9 +1,90 @@
 #include <Windows.h>
+#include <cstdint>
+
+// ウィンドウプロシージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	// メッセージに応じてゲーム固有の処理を行う
+	switch (msg)
+	{
+		// ウィンドウが破棄された
+		case WM_DESTROY:
+			// OSに対して、アプリの終了を伝える
+			PostQuitMessage(0);
+			break;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// 出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello,DirextX!\n");
+	
+	WNDCLASS wc{};
+	// ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	// ウィンドウクラス名(なんでもいい)
+	wc.lpszClassName = L"CG2WindowClass";
+	// インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	// カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
 
+	// ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+
+	// クライアント領域のサイズ
+	const int32_t kClientwidth = 1280;
+	const int32_t kClientheight = 720;
+
+	// ウィンドウサイズを表す構造体にクライアント領域を入れる
+	RECT wrc = { 0,0,kClientwidth,kClientheight };
+	
+	// クライアント領域を元に実際のサイズにwrcを変更してもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	
+
+	// ウィンドウの生成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,		// 利用するクラス名
+		L"CG2",					// タイトルバーの文字（何でもいい）
+		WS_OVERLAPPEDWINDOW,	// よく見るウィンドウスタイル
+		CW_USEDEFAULT,			// 表示X座標（Windowsに任せる）
+		CW_USEDEFAULT,			// 表示Y座標（WindowsOSに任せる）
+		wrc.right - wrc.left,	// ウィンドウ横幅
+		wrc.bottom - wrc.top,	// ウィンドウ縦幅
+		nullptr,				// 親ウィンドウハンドル
+		nullptr,				// メニューハンドル
+		wc.hInstance,			// インスタンスハンドル
+		nullptr);				// オプション
+
+	// ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+
+
+	//////////////////////////////////////
+	/////　　　　初期化処理ここまで　　　　/////
+	//////////////////////////////////////
+
+	MSG msg{};
+	// ウィンドウの×ボタンが押されるまでループ
+	while (msg.message != WM_QUIT) {
+		// Windowにメッセージが来てたら最優先で処理させる
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			// ゲームの処理
+		}
+	}
+
+
+	
 	return 0;
 }
+
