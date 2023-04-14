a27a65bbd2a83253870c18fe467c2f228110ec63
祐介 戸田 <k022g0025@m.neec.ac.jp>
2023-04-14 15:09:05 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	// 出力ウィンドウへの文字出力
+	OutputDebugStringA("Hello,DirextX!\n");
+
+	return 0;
+}
