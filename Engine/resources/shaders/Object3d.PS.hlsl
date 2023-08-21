c5eb686c83b8a55a93945004c0f60e6ef47eb690
祐介 戸田 <k022g0025@m.neec.ac.jp>
2023-08-21 21:07:47 +0900

    Half Lambert実装

--- Engine/resources/shaders/Object3d.PS.hlsl
+++ Engine/resources/shaders/Object3d.PS.hlsl
@@ -4,7 +4,9 @@ float32_t4 main(VertexShaderOutput input) : SV_TARGET {
     float32_t4 output;
 
     if (gMaterial.enableLighting != 0) { // Lightingの計算を行う
-        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+        //float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
         output = input.color * gTexture.Sample(gSampler, input.texcoord) * gDirectionalLight.color * cos * gDirectionalLight.intensity;
     }
     else { // Lightingの計算を行わない
