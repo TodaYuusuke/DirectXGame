be22ba360598c3b363ea171b2ab5e8c343c9e8ae
k022g0038@g.neec.ac.jp <k022g0038@g.neec.ac.jp>
2023-10-26 04:37:13 +0900

    リザルトにスコアとタイムのイージング表示を実装

--- Engine/scene/user/GameResult.h
+++ Engine/scene/user/GameResult.h
@@ -4,7 +4,7 @@
 class GameResult final
 	: public IScene {
 public:
-	GameResult() = default;
+	GameResult(int32_t score, int32_t time);
 	~GameResult() = default;
 
 	//*** 純粋仮想関数の実体宣言 ***//
@@ -21,27 +21,40 @@ public:
 private: //*** これより先に必要な処理や変数を記述 ***//
 
 	// テクスチャ
+	LWP::Resource::Texture* backgroundTex_ = nullptr;
 	LWP::Resource::Texture* transitionTex_ = nullptr;
 	LWP::Resource::Texture* buttonTex_ = nullptr;
 
 	// モデル
 
+	//	背景用の平面
+	LWP::Primitive::Surface* background_ = nullptr;
 
 	// シーン遷移用の平面
 	LWP::Primitive::Surface* transitionSurfase_[2] = { nullptr };
 
 	// リザルト用
-	LWP::Primitive::Surface* resultScore_ = nullptr;
-	LWP::Primitive::Surface* resultTime_ = nullptr;
-	LWP::Primitive::Surface* result_ = nullptr;
+	LWP::Primitive::Surface* titleButton_ = nullptr;
+	LWP::Primitive::Surface* retriButton_ = nullptr;
 
 	// イージング用ワールドトランスフォーム
-	LWP::Object::WorldTransform scoreWorldTransform_[2];
-	LWP::Object::WorldTransform timeWorldTransform_[2];
-	LWP::Object::WorldTransform resultWorldTransform_[2];
+	LWP::Object::WorldTransform titleWorldTransform_[2];
+	LWP::Object::WorldTransform retriWorldTransform_[2];
 
 	// イージング用タイム
 	float t[3] = { 0.0f };
+
+	// 数値テクスチャ
+	LWP::Resource::Texture* numberTex_[10] = { nullptr };
+	// スコア
+	int32_t score_;
+	int32_t viewScore_;
+	LWP::Primitive::Surface* scoreUI_[8] = { nullptr };
+	// タイム
+	int32_t time_;
+	int32_t viewTime;
+	LWP::Primitive::Surface* timeUI_[3] = { nullptr };
+	int32_t nextSceneWaitTime_;
 
 	// タイトルかリトライの選択ボタン
 	bool isGoTitle = false;
