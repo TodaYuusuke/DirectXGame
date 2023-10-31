475bed8c216b9eddeb6f5c3e098deb440281f54a
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2023-10-31 09:47:55 +0900

    observerクラスのファイルを作成（まだ未実装）

---
+++ Engine/utility/Observer.h
@@ -0,0 +1,34 @@
+#pragma once
+namespace LWP::Utility {
+	/// <summary>
+	/// 変数の変化を監視するオブザーバーデザインパターン用のクラス
+	/// </summary>
+	template <typename T>
+	class Observer {
+	public:
+		// ** パブリックなメンバ変数 ** //
+		
+		// 変数
+		T t;
+		// 関数ポインタ
+		auto* funcPtr;
+
+
+		// ** オペレーターオーバーロード ** //
+
+		// Observer Add(+) T
+		Observer operator+(const T& other) const;
+		Observer& operator+=(const T& other);
+		// Observer Subtract(-) T
+		Observer operator-(const T& other) const;
+		Observer& operator-=(const T& other);
+		// Observer Multiply(*) T
+		Observer operator*(const T& other) const;
+		Observer& operator*=(const T& other);
+		// Observer Division(/) T
+		Observer operator/(const T& other) const;
+		Observer& operator/=(const T& other);
+		// Observer Equal(=) T
+		Observer operator=(const T& other) const;
+	};
+}
