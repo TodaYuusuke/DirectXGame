f53509cff1118afb500713dba99b03d3630622dd
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-01-11 11:38:44 +0900

    パラメータクラス宣言

---
+++ Engine/resources/parameter/Parameter.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace LWP::Resource {
+	class Parameter final {
+	public:
+		// ** 共通の変数 ** //
+
+		// コンストラクタ
+		Parameter() = default;
+		// デストラクタ
+		~Parameter() = default;
+
+	};
+}
