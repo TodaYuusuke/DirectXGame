ab38faa0b09b066b5f098972b47b2ea254ae1e02
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-04-24 15:28:22 +0900

    libビルドバッチをエンジンファイル内に用意

---
+++ tool/bat/LWP_ReBuild.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Visual Studio Developer Command Promptを使用する場合
+REM call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+
+cd "../../"
+
+REM Debugでビルド
+MSBuild "DirectXGame.sln" /t:clean;rebuild /p:ConfigurationType=StaticLibrary;Configuration=Debug;MultiProcessorCompilation=true
+REM DEMOでビルド
+REM MSBuild "DirectXGame.sln" /t:clean;rebuild /p:ConfigurationType=StaticLibrary;Configuration=Release;MultiProcessorCompilation=true
+REM Releaseでビルド
+MSBuild "DirectXGame.sln" /t:clean;rebuild /p:ConfigurationType=StaticLibrary;Configuration=Release;MultiProcessorCompilation=true
+
+REM libを浅い階層にコピー
+REM xcopy "x64/Debug/DirectXGame.lib" "../lib/LWP_Debug.lib" /y
+REM xcopy "x64/Demo/DirectXGame.lib" "../lib/LWP_Demo.lib" /y
+REM xcopy "x64/Release/DirectXGame.lib" "../lib/LWP_Release.lib" /y
+
+REM システムのリソースをコピー
+xcopy "resources/system" "../resources/system\" /e /y
+
+pause
---
+++ tool/bat/LWP_Update.bat
@@ -0,0 +1,33 @@
+@echo off
+
+REM Visual Studio Developer Command Promptを使用する場合
+REM call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+
+cd "../../"
+
+REM 変更をすべて取り消す
+git reset --hard
+REM Gitで最新のコードを取得
+git pull origin master
+if ERRORLEVEL 1 (
+    REM ここにエラー発生時の処理を記述します
+    echo.
+    echo git command could not be executed.
+    echo Please check if git is installed.
+    echo Please ask your administrator for error details.
+    echo.
+    pause
+    exit /b
+)
+
+
+REM Debugでビルド
+MSBuild "DirectXGame.sln" /t:clean;rebuild /p:ConfigurationType=StaticLibrary;Configuration=Debug;MultiProcessorCompilation=true
+REM Releaseでビルド
+MSBuild "DirectXGame.sln" /t:clean;rebuild /p:ConfigurationType=StaticLibrary;Configuration=Release;MultiProcessorCompilation=true
+
+REM システムのリソースをコピー
+xcopy "resources/system" "../resources/system\" /e /y
+
+pause
