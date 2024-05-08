2706d90cef2eef86542339bfdb65e3cc4ed8f95e
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-05-08 15:41:20 +0900

    シェーダーファイルコピー

--- resources/system/shaders/postProcess/PostProcess.PS.hlsl
+++ resources/system/shaders/postProcess/PostProcess.PS.hlsl
@@ -1,23 +1,8 @@
 #include "PassThrough.hlsli"
 
-// パラメータ
-struct Parameter
-{
-    int time; // !< 時間
-    int rWidth; // !< 解像度（横幅）
-    int rHeight; // !< 解像度（縦幅） 
-};
-
-// パラメータのバインド
-ConstantBuffer<Parameter> gPara : register(b0);
-// ポストプロセスをかけるテクスチャ（ちゃんとTexture2D二枚で渡せるようにいつか修正する）
-// 0 ... 深度マップ
-// 1 ... レンダリング結果
-Texture2D<float32_t4> gTexture[] : register(t0);
-//Texture2D<float32_t4> gDepth : register(t1);
-// サンプラー
-SamplerState gSampler : register(s0);
-
+float32_t3 GrayScale(float32_t3 color) {
+    return dot(color, float32_t3(0.2125f, 0.7154f, 0.0721f));
+}
 
 float32_t4 main(PSInput input) : SV_TARGET
 {
@@ -28,7 +13,9 @@ float32_t4 main(PSInput input) : SV_TARGET
     float2 uv = input.texcoord;
 
 	// サンプリング
-    output = gTexture[1].Sample(gSampler, uv);
-
+    output = gTexture.Sample(gSampler, uv);
+    // グレースケール化
+    output.rgb = GrayScale(output.rgb);
+    
     return output;
 }
