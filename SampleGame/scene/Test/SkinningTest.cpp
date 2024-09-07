b35ee8472bf5f90632998bba670f88252e617c10
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-09-08 01:59:00 +0900

    Skinningテスト用のシーン作成

---
+++ SampleGame/scene/Test/SkinningTest.cpp
@@ -0,0 +1,18 @@
+#include "SkinningTest.h"
+
+using namespace LWP;
+using namespace LWP::Input;
+using namespace LWP::Primitive;
+using namespace LWP::Math;
+using namespace LWP::Utility;
+using namespace LWP::Object;
+
+// 初期化
+void SkinningTest::Initialize() {
+	model.LoadShortPath("SkinningTestModel/Player_Boned_IK.gltf");
+	anim.Load
+}
+
+// 更新
+void SkinningTest::Update() {
+}
