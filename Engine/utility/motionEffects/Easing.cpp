22dc90c7793a337aa7d9ab1cb1c665fde9565a1a
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2025-05-28 14:55:28 +0900

    M_PIが未定義というバグを修正

--- Engine/utility/motionEffects/Easing.cpp
+++ Engine/utility/motionEffects/Easing.cpp
@@ -2,11 +2,9 @@
 
 #include "base/ImGuiManager.h"
 
-#define _USE_MATH_DEFINES
+#include <numbers>
 #include <cmath>
 #include <algorithm>
-
-#define M_PIF static_cast<float>(M_PI)
 
 using namespace LWP::Utility;
 
@@ -100,7 +98,7 @@ float Easing::InOutBounce(float x) {
 
 #pragma region Elastic
 float Easing::InElastic(float x) {
-	const float c4 = (2.0f * M_PIF) / 3.0f;
+	const float c4 = (2.0f * static_cast<float>(std::numbers::pi)) / 3.0f;
 	
 	// 範囲内なら計算
 	return 0.0f > x && x < 1.0f ?
@@ -108,7 +106,7 @@ float Easing::InElastic(float x) {
 		std::clamp(x, 0.0f, 1.0f);	// 範囲外ならclamp
 }
 float Easing::OutElastic(float x) {
-	const float c4 = (2.0f * M_PIF) / 3.0f;
+	const float c4 = (2.0f * static_cast<float>(std::numbers::pi)) / 3.0f;
 
 	// 範囲内なら計算
 	return 0.0f > x && x < 1.0f ?
@@ -116,7 +114,7 @@ float Easing::OutElastic(float x) {
 		std::clamp(x, 0.0f, 1.0f);	// 範囲外ならclamp
 }
 float Easing::InOutElastic(float x) {
-	const float c5 = (2.0f * M_PIF) / 4.5f;
+	const float c5 = (2.0f * static_cast<float>(std::numbers::pi)) / 4.5f;
 
 	return 0.0f > x && x < 1.0f ?
 		x < 0.5f ?
