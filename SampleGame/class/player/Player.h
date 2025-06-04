a699b12fee2358af89f01c439436ff88278fbc48
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2025-06-05 02:43:13 +0900

    ヘッダーだけマージされていない問題を修正

--- SampleGame/class/player/Player.h
+++ SampleGame/class/player/Player.h
@@ -23,14 +23,23 @@ public:
 
 private: // ** メンバ定数 ** //
 
+	// 重力加速度
+	float kGravityAcce = 9.8f;
+	// プレイヤーの加速度
+	float kWalkSpeed = 3.0f;
+	// 速度の減衰率
+	float kDecayRate = 0.8f;
+
 	// カメラの回転速度
 	float kCameraRotationSpeed = 0.011f;
 
 
 private: // ** メンバ変数 ** //
-	
+
 	// カメラのポインタ
 	LWP::Object::Camera* camera_ = nullptr;
+	// 速度
+	LWP::Math::Vector3 velocity = { 0.0f,0.0f,0.0f };
 
 	// 銃のデータ
 	Pistol pistol_;
@@ -38,6 +47,10 @@ private: // ** メンバ変数 ** //
 
 private: // ** カメラ系処理 ** // 
 
+	/// <summary>
+	/// 移動処理
+	/// </summary>
+	void Move();
 	/// <summary>
 	/// カメラ回転処理
 	/// </summary>
